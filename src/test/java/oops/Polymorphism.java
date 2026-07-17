23cd56096197c9cf857e50250e36759e7828211c
learnautomation1604-cyber <learnautomation1604@gmail.com>
2026-07-17 23:48:46 +0530

    todayscommitforJava=Aray in detail_07172026

---
+++ src/test/java/oops/Polymorphism.java
@@ -0,0 +1,10 @@
+package oops;
+
+public class Polymorphism {
+
+    // Polymorphism it's a OOPS concept that allows One method or Object take many morphism
+    // Types of polymorphism -
+    // 1. Method Overlading or Compile time polymorphism
+    // 2. Method Pverridding - Run time Polymorphism
+
+   }
